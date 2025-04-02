adbe8e33118656d77cd0c5536058ba13d10d2ecd
kumuduamarakoon <kumudu4746@gmail.com>
2025-04-02 22:59:43 +0530

    Update Add Medical Form

---
+++ JavaLMS/src/main/java/com/mycompany/javalms/JavaLMS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javalms;
+
+/**
+ *
+ * @author Emil Laptop
+ */
+public class JavaLMS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
