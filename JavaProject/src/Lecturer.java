9be82a9aa62fa1ebd5645aa2d177a4c1beb888be
Pawan Thathsara <pawanthathsara987@gmail.com>
2025-04-05 17:24:28 +0530

    Add Lecture Materials page updated

---
+++ JavaProject/src/Lecturer.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Lecturer {
+    private JPanel panel1;
+    private JButton addLectureMaterialButton;
+    private JButton viewMedicalButton;
+    private JButton viewTimeTableButton;
+    private JButton addMarksButton;
+    private JButton viewStudentDetailsButton;
+    private JButton viewStudentEligibilityButton;
+    private JComboBox comboBox1;
+    private JButton uploadFileButton;
+    private JTextField textField1;
+    private JButton uploadButton;
+}
