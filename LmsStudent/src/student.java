57e53e5cbb11c6f720379628c4249e62cbcd4db6
supun <supun4550@gmail.com>
2025-04-10 00:09:39 +0530

    Update profile page

---
+++ LmsStudent/src/student.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class student {
+    private JPanel mainPanal;
+    private JPanel L_student;
+    private JButton updateProfileButton;
+    private JButton viewGradeButton1;
+    private JButton viewAttendansButton;
+    private JButton viewMarksButton;
+    private JButton viewNoticeButton;
+    private JComboBox comboBox1;
+    private JButton updateProfilePictureButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton saveChangesButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
