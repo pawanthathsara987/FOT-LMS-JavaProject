70f11a1d532ea7f35d01159459855ac7a9130170
Pawan Thathsara <pawanthathsara987@gmail.com>
2025-04-15 00:00:32 +0530

    Add marks page and Update marks pages updated with database.

--- JavaProject/src/Lecturer.java
+++ JavaProject/src/Lecturer.java
@@ -1,5 +1,3 @@
-import org.w3c.dom.ls.LSOutput;
-
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
@@ -11,22 +9,22 @@ import java.util.Map;
 public class Lecturer {
     private JPanel MainPanel;
     private JButton addLectureMaterialButton;
-    private JButton viewMedicalButton;
-    private JButton viewTimeTableButton;
-    private JButton addMarksButton;
     private JButton viewStudentDetailsButton;
+    private JButton viewTimeTableButton;
+    private JButton UpdateMarksButton;
+    private JButton addMarksButton;
     private JButton viewStudentEligibilityButton;
     private JComboBox comboBox1;
     private JButton uploadFileButton;
     private JTextField textField1;
     private JButton uploadButton;
-    private JComboBox markTypecomboBox;
+    private JComboBox updateMarkTypeComboBox;
     private JPanel addLecMaterialPanel;
+    private JPanel updateMarksPanel;
     private JPanel addMarksPanel;
-    private JPanel viewStuDetailsPanel;
     private JPanel viewStuEligibilityPanel;
     private JPanel viewTimeTablePanel;
-    private JPanel viewMedicalPanel;
+    private JPanel viewStudentDetailsPanel;
     private JPanel parentPanel;
     private JPanel sidePanel;
     private JPanel lecturerLabel;
@@ -36,23 +34,41 @@ public class Lecturer {
     private JTextField enteredMarksField;
     private JPanel marksTypePanel;
     private JPanel subjectDisplayPanel;
-    private JPanel addMarksTitlePanel;
+    private JPanel updateMarksTitlePanel;
     private JPanel marksEnterPanel;
     private JPanel marksPanel;
-    private JButton addMarksButton1;
+    private JButton updateMarksButton1;
     private JLabel studentIDLabel;
     private JPanel studentIDLabelPanel;
     private JPanel studentIDTextFieldPanel;
-    private JPanel addMarksButtonPanel;
+    private JPanel updateMarksButtonPanel;
     private JPanel timeTablePanel;
     private JPanel studentEligibilityPanel;
     private JTable studentDetailsTable;
-    private JPanel stuDetailsTablePanel;
     private JTextField stuIDField;
+    private JPanel stuDetailsTablePanel;
+    private JPanel addMarksLabelPanel;
+    private JPanel subjectPanel;
+    private JPanel stuIDPanel;
+    private JTextField stuIDTextField;
+    private JTextField stuCourseTextField;
+    private JComboBox addMarkTypeComboBox;
+    private JPanel markTypePanel;
+    private JPanel courseIDPanel;
+    private JPanel enterMarksPanel;
+    private JPanel addMarksButtonPanel;
+    private JPanel addStuMarksTable;
+    private JButton addMarksButton1;
+    private JTextField stuMarksField;
+    private JTable addStudentMarksTable;
 
     private String stuid;
     private String marksValue;
     private String markType;
+    private String courseID;
+    private String stuid2;
+    private String marksValue2;
+    private String markType2;
 
     private Connection conn = null;
     private Statement stmt = null;
@@ -77,6 +93,17 @@ public class Lecturer {
                 parentPanel.revalidate();
             }
         });
+
+        UpdateMarksButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                parentPanel.removeAll();
+                parentPanel.add(updateMarksPanel);
+                parentPanel.repaint();
+                parentPanel.revalidate();
+            }
+        });
+
         addMarksButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -86,15 +113,7 @@ public class Lecturer {
                 parentPanel.revalidate();
             }
         });
-        viewStudentDetailsButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                parentPanel.removeAll();
-                parentPanel.add(viewStuDetailsPanel);
-                parentPanel.repaint();
-                parentPanel.revalidate();
-            }
-        });
+
         viewStudentEligibilityButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -104,6 +123,7 @@ public class Lecturer {
                 parentPanel.revalidate();
             }
         });
+
         viewTimeTableButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -113,22 +133,23 @@ public class Lecturer {
                 parentPanel.revalidate();
             }
         });
-        viewMedicalButton.addActionListener(new ActionListener() {
+
+        viewStudentDetailsButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 parentPanel.removeAll();
-                parentPanel.add(viewMedicalPanel);
+                parentPanel.add(viewStudentDetailsPanel);
                 parentPanel.repaint();
                 parentPanel.revalidate();
             }
         });
 
-        addMarksButton1.addActionListener(new ActionListener() {
+        updateMarksButton1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 stuid = stuIDField.getText().trim();
                 marksValue = enteredMarksField.getText();
-                markType = markTypecomboBox.getSelectedItem().toString();
+                markType = updateMarkTypeComboBox.getSelectedItem().toString();
 
                 if (stuid.isEmpty() || marksValue.isEmpty() || markType.isEmpty()) {
                     JOptionPane.showMessageDialog(parentPanel, "Please fill all fields!", "Error", JOptionPane.ERROR_MESSAGE);
@@ -142,7 +163,6 @@ public class Lecturer {
                 } catch (NumberFormatException ex) {
                     JOptionPane.showMessageDialog(null, "Please enter a valid number for marks.");
                 }
-
             }
 
             public void updateStuMarks(String stuid, double marks, String markType){
@@ -220,7 +240,7 @@ public class Lecturer {
 
                     stuIDField.setText("");
                     enteredMarksField.setText("");
-                    markTypecomboBox.setSelectedIndex(0);
+                    updateMarkTypeComboBox.setSelectedIndex(0);
 
                 } catch (SQLException e) {
                     System.out.println("Statement error" + e.getMessage());
@@ -228,6 +248,106 @@ public class Lecturer {
             }
         });
 
+        addMarksButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                stuid2 = stuIDTextField.getText().trim();
+                courseID = stuCourseTextField.getText().trim();
+                markType2 = addMarkTypeComboBox.getSelectedItem().toString();
+                marksValue2 = stuMarksField.getText();
+
+                if (stuid2.isEmpty() || marksValue2.isEmpty() || markType2.isEmpty() || courseID.isEmpty()) {
+                    JOptionPane.showMessageDialog(parentPanel, "Please fill all fields!", "Error", JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+
+                try{
+                    double marks = Double.parseDouble(marksValue2);
+                    addStuMarks(stuid2,courseID,markType2,marks);
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null, "Please enter a valid number for marks.");
+                }
+            }
+
+            public void addStuMarks(String stuid2, String courseID, String markType2, double marks){
+                DbConnector db = new DbConnector();
+                conn = db.getConnection();
+
+                Map<String,String> columns = new HashMap<>();
+                columns.put("Quiz 1","quiz_1");
+                columns.put("Quiz 2","quiz_2");
+                columns.put("Quiz 3","quiz_3");
+                columns.put("Assignment Marks","assesment");
+                columns.put("Mid Exam Marks","m_marks");
+                columns.put("End Theory Marks","f_theory");
+                columns.put("End Practical Marks","f_practical");
+
+                String columnName = columns.get(markType2);
+
+                if (columnName != null) {
+                    String sql = "INSERT INTO mark(stuid,ccode," + columnName + ") VALUES (?,?,?)";
+                    try {
+                        PreparedStatement pstmt = conn.prepareStatement(sql);
+                        pstmt.setString(1, stuid2);
+                        pstmt.setString(2, courseID);
+                        pstmt.setDouble(3, marks);
+                        int rows = pstmt.executeUpdate();
+                        if (rows > 0) {
+                            loadAddMarksTable();
+                            JOptionPane.showMessageDialog(null, "Mark Insertion successfully!", "Success", JOptionPane.INFORMATION_MESSAGE);
+                        }
+                    } catch (SQLException e) {
+                        System.out.println("Statement error: " + e.getMessage());
+                        JOptionPane.showMessageDialog(null, "Database error: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            }
+
+            public void loadAddMarksTable(){
+                DbConnector db = new DbConnector();
+                conn = db.getConnection();
+
+                System.out.println("loadMarks() called...");
+
+                String sql = "select * from mark";
+
+                try {
+                    PreparedStatement pstmt = conn.prepareStatement(sql);
+                    ResultSet rs = pstmt.executeQuery();
+
+                    String[] columnNames2 = {"Stu_ID", "Cour_Code", "Quiz 1", "Quiz 2", "Quiz 3", "Assignments", "Mid Marks", "End Theory", "End Practical"};
+                    DefaultTableModel model2 = new DefaultTableModel(columnNames2,0);
+
+                    while (rs.next()) {
+                        Object[] row = {
+                              rs.getString("stuid"),
+                              rs.getString("ccode"),
+                              rs.getDouble("quiz_1"),
+                              rs.getDouble("quiz_2"),
+                              rs.getDouble("quiz_3"),
+                              rs.getDouble("assesment"),
+                              rs.getDouble("m_marks"),
+                              rs.getDouble("f_theory"),
+                              rs.getDouble("f_practical")
+                        };
+                        model2.addRow(row);
+                    }
+
+                    addStudentMarksTable.setModel(model2);
+                    addStudentMarksTable.repaint();
+                    addStudentMarksTable.revalidate();
+
+                    stuIDTextField.setText("");
+                    stuMarksField.setText("");
+                    stuCourseTextField.setText("");
+                    addMarkTypeComboBox.setSelectedIndex(0);
+
+                } catch (SQLException e) {
+                    System.out.println("Statement error: " + e.getMessage());
+                }
+
+            }
+        });
     }
 
     public void studentDetailsTable(){
