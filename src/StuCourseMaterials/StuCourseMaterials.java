bd35db031cb7683944896b135c324c7f0a775b98
KPGSSandaruwan <supun4550@gmail.com>
2025-04-24 02:07:47 +0530

    create course materials

---
+++ src/StuCourseMaterials/StuCourseMaterials.java
@@ -0,0 +1,12 @@
+package StuCourseMaterials;
+
+import javax.swing.*;
+
+public class StuCourseMaterials {
+    private JPanel rootPanel;
+    private JPanel mainPanel;
+    private JPanel titlepanel;
+    private JPanel contentPanel;
+    private JComboBox weeks;
+    private JTable table1;
+}
